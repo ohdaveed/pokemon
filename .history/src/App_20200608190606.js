0d3da566fc480651d269849e880d4246e3d31c21
David Arrizon <arrizon.david@gmail.com>
2020-06-08 19:08:12 -0500

    can add and remove pokemon

--- .history/src/App_20200608190606.js
+++ .history/src/App_20200608190606.js
@@ -3,14 +3,14 @@ import React from "react";
 import { PokemonProvider } from "./PokemonContext";
 
 import PokemonsList from "./PokemonsList";
-// import Pokedex from "./Pokedex";
+import Pokedex from "./Pokedex";
 import CapturedPokemons from "./CapturedPokemons.js";
 
 const App = () => (
   <PokemonProvider>
     <div className="main">
       <PokemonsList />
-      
+      <Pokedex />
       <CapturedPokemons />
     </div>
     <div className="form-wrapper"></div>
